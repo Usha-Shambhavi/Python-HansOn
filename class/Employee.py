13cf5378040cfc45be98941a91aa7022f505b5fa
Zubair <syedzubair89@gmail.com>
2025-06-12 07:52:02 +0530

    Draft about Manager class

--- class/Employee.py
+++ class/Employee.py
@@ -20,3 +20,9 @@ class Employee:
             return True
         return False
     
+'''
+    class Manager (Employee):
+        def __init__(self, name, e_Id, dept="Management", salary, team_size =0 ):
+            super().__init__(name, e_Id, dept, salary)
+            self.team_size = team_size
+'''    
