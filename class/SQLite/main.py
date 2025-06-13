1155b91e9b89f81a7ab9c8bedd6fcbc922bedbc2
Usha-Shambhavi <usha.mahesharya@gmail.com>
2025-06-13 07:59:10 +0530

    update database

--- class/SQLite/main.py
+++ class/SQLite/main.py
@@ -3,5 +3,9 @@ import sqlite3
 from sqlExample import SQLExample
 
 connection = sqlite3.connect("EmployeeDB.db")
-emp = SQLExample('Bharat',678,178000)
+emp = SQLExample('Usha',678,178000)
+emp = SQLExample('Ntihin',123,178000)
+emp = SQLExample('Bharat',456,178000)
+emp = SQLExample('Syed',890,178000)
 #emp.getEmployees(connection)
+ 
