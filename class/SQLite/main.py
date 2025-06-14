89b22dc216884cc8af12ebeea3d0f650d2385b73
Zubair <syedzubair89@gmail.com>
2025-06-14 07:59:28 +0530

    added code

--- class/SQLite/main.py
+++ class/SQLite/main.py
@@ -3,5 +3,13 @@ import sqlite3
 from sqlExample import SQLExample
 
 connection = sqlite3.connect("EmployeeDB.db")
-emp = SQLExample('Bharat',678,178000)
+#emp = SQLExample('Bharat',678,178000)
 #emp.getEmployees(connection)
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
+plt.plot(x, y)
+plt.title("Sine Wave")
+plt.show()
